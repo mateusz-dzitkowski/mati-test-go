812031ed6dfd86ce9506475dc6b292db88f725bd
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-09-21 23:35:35 +0200

    damn I didn't do a good job with this one

---
+++ aoc-2015/05.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"aoc_2015/utils"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	data := utils.LoadExercise("05")
+	fmt.Println(solve052(data))
+}
+
+func solve051(data string) int {
+	var (
+		vowels         = []string{"a", "e", "i", "o", "u"}
+		naughtyStrings = []string{"ab", "cd", "pq", "xy"}
+		repeats        = []string{"aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"}
+	)
+
+	total := 0
+
+	for _, line := range strings.Split(data, "\n") {
+		if countContains(line, vowels) >= 3 && countContains(line, repeats) >= 1 && countContains(line, naughtyStrings) < 1 {
+			total += 1
+		}
+	}
+
+	return total
+}
+
+func solve052(data string) int {
+	total := 0
+
+	alphabet := strings.Split("abcdefghijklmnopqrstuvwxyz", "")
+	betweens := make([]string, len(alphabet)*len(alphabet))
+	index := 0
+	for _, let := range alphabet {
+		for _, ter := range alphabet {
+			betweens[index] = fmt.Sprintf("%s%s%s", let, ter, let)
+			index += 1
+		}
+	}
+
+	for _, line := range strings.Split(data, "\n") {
+		if hasPair(line) && countContains(line, betweens) > 0 {
+			total += 1
+		}
+	}
+
+	return total
+}
+
+func countContains(s string, substrings []string) int {
+	total := 0
+
+	for _, substring := range substrings {
+		total += strings.Count(s, substring)
+	}
+
+	return total
+}
+
+func hasPair(s string) bool {
+	pairs := make([]string, len(s)-1)
+	for i := range pairs {
+		pairs[i] = s[i : i+2]
+	}
+
+	for i := 0; i < len(pairs)-2; i++ {
+		for j := i + 2; j < len(pairs); j++ {
+			if pairs[i] == pairs[j] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
